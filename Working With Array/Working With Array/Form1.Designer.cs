8ae513b934d249d4bad0b1512c7916ca1095fb63
Trần Quang Đãng <91641923+quangdang46@users.noreply.github.com>
2022-05-05 09:51:02 +0700

    Add files via upload

---
+++ Working With Array/Working With Array/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace Working_With_Array
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnXuat = new System.Windows.Forms.Button();
+            this.btnSum = new System.Windows.Forms.Button();
+            this.btnDemLe = new System.Windows.Forms.Button();
+            this.btnSumLe = new System.Windows.Forms.Button();
+            this.btnMin = new System.Windows.Forms.Button();
+            this.btnAdd2 = new System.Windows.Forms.Button();
+            this.btnTang = new System.Windows.Forms.Button();
+            this.btnGiam = new System.Windows.Forms.Button();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtInput);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(37, 22);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(406, 64);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Mảng gốc:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtOutput);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(37, 92);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(406, 61);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Kết quả:";
+            // 
+            // btnXuat
+            // 
+            this.btnXuat.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnXuat.Location = new System.Drawing.Point(37, 170);
+            this.btnXuat.Name = "btnXuat";
+            this.btnXuat.Size = new System.Drawing.Size(190, 34);
+            this.btnXuat.TabIndex = 1;
+            this.btnXuat.Text = "Xuất mảng ngẫu nhiên";
+            this.btnXuat.UseVisualStyleBackColor = true;
+            this.btnXuat.Click += new System.EventHandler(this.btnXuat_Click);
+            // 
+            // btnSum
+            // 
+            this.btnSum.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSum.Location = new System.Drawing.Point(37, 212);
+            this.btnSum.Name = "btnSum";
+            this.btnSum.Size = new System.Drawing.Size(190, 34);
+            this.btnSum.TabIndex = 1;
+            this.btnSum.Text = "Tính tổng giá trị mảng";
+            this.btnSum.UseVisualStyleBackColor = true;
+            this.btnSum.Click += new System.EventHandler(this.btnSum_Click);
+            // 
+            // btnDemLe
+            // 
+            this.btnDemLe.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDemLe.Location = new System.Drawing.Point(37, 254);
+            this.btnDemLe.Name = "btnDemLe";
+            this.btnDemLe.Size = new System.Drawing.Size(190, 34);
+            this.btnDemLe.TabIndex = 1;
+            this.btnDemLe.Text = "Đếm số phần tử lẻ";
+            this.btnDemLe.UseVisualStyleBackColor = true;
+            this.btnDemLe.Click += new System.EventHandler(this.btnDemLe_Click);
+            // 
+            // btnSumLe
+            // 
+            this.btnSumLe.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSumLe.Location = new System.Drawing.Point(37, 298);
+            this.btnSumLe.Name = "btnSumLe";
+            this.btnSumLe.Size = new System.Drawing.Size(190, 34);
+            this.btnSumLe.TabIndex = 1;
+            this.btnSumLe.Text = "Tổng giá trị phần tử lẻ";
+            this.btnSumLe.UseVisualStyleBackColor = true;
+            this.btnSumLe.Click += new System.EventHandler(this.btnSumLe_Click);
+            // 
+            // btnMin
+            // 
+            this.btnMin.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMin.Location = new System.Drawing.Point(281, 170);
+            this.btnMin.Name = "btnMin";
+            this.btnMin.Size = new System.Drawing.Size(162, 34);
+            this.btnMin.TabIndex = 1;
+            this.btnMin.Text = "Tìm phần tử nhỏ nhất";
+            this.btnMin.UseVisualStyleBackColor = true;
+            this.btnMin.Click += new System.EventHandler(this.btnMin_Click);
+            // 
+            // btnAdd2
+            // 
+            this.btnAdd2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd2.Location = new System.Drawing.Point(281, 212);
+            this.btnAdd2.Name = "btnAdd2";
+            this.btnAdd2.Size = new System.Drawing.Size(162, 34);
+            this.btnAdd2.TabIndex = 1;
+            this.btnAdd2.Text = "Tăng mỗi phần tử lên 2";
+            this.btnAdd2.UseVisualStyleBackColor = true;
+            this.btnAdd2.Click += new System.EventHandler(this.btnAdd2_Click);
+            // 
+            // btnTang
+            // 
+            this.btnTang.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTang.Location = new System.Drawing.Point(281, 254);
+            this.btnTang.Name = "btnTang";
+            this.btnTang.Size = new System.Drawing.Size(162, 34);
+            this.btnTang.TabIndex = 1;
+            this.btnTang.Text = "Sắp xếp mảng tăng";
+            this.btnTang.UseVisualStyleBackColor = true;
+            this.btnTang.Click += new System.EventHandler(this.btnTang_Click);
+            // 
+            // btnGiam
+            // 
+            this.btnGiam.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGiam.Location = new System.Drawing.Point(281, 298);
+            this.btnGiam.Name = "btnGiam";
+            this.btnGiam.Size = new System.Drawing.Size(162, 34);
+            this.btnGiam.TabIndex = 1;
+            this.btnGiam.Text = "Sắp xếp mảng giảm";
+            this.btnGiam.UseVisualStyleBackColor = true;
+            this.btnGiam.Click += new System.EventHandler(this.btnGiam_Click);
+            // 
+            // txtInput
+            // 
+            this.txtInput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInput.Location = new System.Drawing.Point(3, 19);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(400, 42);
+            this.txtInput.TabIndex = 0;
+            this.txtInput.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtInput.TextChanged += new System.EventHandler(this.txtInput_TextChanged);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOutput.Location = new System.Drawing.Point(3, 19);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(400, 39);
+            this.txtOutput.TabIndex = 0;
+            this.txtOutput.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(494, 358);
+            this.Controls.Add(this.btnGiam);
+            this.Controls.Add(this.btnSumLe);
+            this.Controls.Add(this.btnTang);
+            this.Controls.Add(this.btnDemLe);
+            this.Controls.Add(this.btnAdd2);
+            this.Controls.Add(this.btnSum);
+            this.Controls.Add(this.btnMin);
+            this.Controls.Add(this.btnXuat);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Button btnXuat;
+        private System.Windows.Forms.Button btnSum;
+        private System.Windows.Forms.Button btnDemLe;
+        private System.Windows.Forms.Button btnSumLe;
+        private System.Windows.Forms.Button btnMin;
+        private System.Windows.Forms.Button btnAdd2;
+        private System.Windows.Forms.Button btnTang;
+        private System.Windows.Forms.Button btnGiam;
+    }
+}
+
---
+++ Working With Array/Working With Array/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Working_With_Array
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtInput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnXuat_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            String tmp = "";
+            int[] arr = new int[10];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rnd.Next(1, 100);
+                tmp += arr[i] + " ";
+            }
+            txtInput.Text = tmp.Trim();
+        }
+
+        private void btnSum_Click(object sender, EventArgs e)
+        {
+            int sum = 0;
+            String[] arr = txtInput.Text.Split(' ');
+            foreach(String s in arr)
+            {
+                sum += int.Parse(s);
+            }
+            txtOutput.Text = sum.ToString();
+        }
+
+        private void btnDemLe_Click(object sender, EventArgs e)
+        {
+            int cnt = 0;
+            String[] arr = txtInput.Text.Split(' ');
+            foreach (String s in arr)
+            {
+                if (int.Parse(s) % 2 != 0)
+                {
+                    cnt++;
+                }
+            }
+            txtOutput.Text = cnt.ToString();
+        }
+
+        private void btnSumLe_Click(object sender, EventArgs e)
+        {
+            int cnt = 0;
+            String[] arr = txtInput.Text.Split(' ');
+            foreach (String s in arr)
+            {
+                if (int.Parse(s) % 2 != 0)
+                {
+                    cnt+= int.Parse(s);
+                }
+            }
+            txtOutput.Text = cnt.ToString();
+        }
+
+        private void btnMin_Click(object sender, EventArgs e)
+        {
+            String[] arr = txtInput.Text.Split(' ');
+            int cnt = int.Parse(arr[0]);
+            foreach (String s in arr)
+            {
+                if (int.Parse(s) < cnt)
+                {
+                    cnt = int.Parse(s);
+                }
+            }
+            txtOutput.Text = cnt.ToString();
+        }
+
+        private void btnAdd2_Click(object sender, EventArgs e)
+        {
+            String tmp = "";
+            String[] arr = txtInput.Text.Split(' ');
+            foreach (String s in arr)
+            {
+                tmp += (int.Parse(s) + 2) + " ";
+            }
+            txtOutput.Text = tmp;
+        }
+
+        private void btnTang_Click(object sender, EventArgs e)
+        {
+            String tmp = "";
+            String[] arr = txtInput.Text.Split(' ');
+            int[] arr2 = new int[arr.Length];
+            for (int i= 0; i < arr.Length; i++)
+            {
+                arr2[i] = int.Parse(arr[i]);
+            }
+            Array.Sort(arr2);
+            for (int i = 0; i < arr2.Length; i++)
+            {
+                tmp += arr2[i] + " ";
+            }
+            txtOutput.Text = tmp.Trim();
+        }
+
+        private void btnGiam_Click(object sender, EventArgs e)
+        {
+            String tmp = "";
+            String[] arr = txtInput.Text.Split(' ');
+            int[] arr2 = new int[arr.Length];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr2[i] = int.Parse(arr[i]);
+            }
+            Array.Sort(arr2);
+            Array.Reverse(arr2);
+            for (int i = 0; i < arr2.Length; i++)
+            {
+                tmp += arr2[i] + " ";
+            }
+            txtOutput.Text = tmp.Trim();
+        }
+    }
+}

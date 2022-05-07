ae4f6813f59a95b891c5d020b71ff7964ccc90d0
Trần Quang Đãng <91641923+quangdang46@users.noreply.github.com>
2022-05-07 22:21:16 +0700

    Add files via upload

---
+++ GameXepHinh/GameXepHinh/frmMain.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace GameXepHinh
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnXepHinh = new System.Windows.Forms.Panel();
+            this.btnTronLaiHinh = new System.Windows.Forms.Button();
+            this.btnChonHinh = new System.Windows.Forms.Button();
+            this.lblHinhGoc = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.lblKiemTra = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // pnXepHinh
+            // 
+            this.pnXepHinh.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnXepHinh.Location = new System.Drawing.Point(18, 18);
+            this.pnXepHinh.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pnXepHinh.Name = "pnXepHinh";
+            this.pnXepHinh.Size = new System.Drawing.Size(400, 400);
+            this.pnXepHinh.TabIndex = 0;
+            // 
+            // btnTronLaiHinh
+            // 
+            this.btnTronLaiHinh.Location = new System.Drawing.Point(306, 426);
+            this.btnTronLaiHinh.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnTronLaiHinh.Name = "btnTronLaiHinh";
+            this.btnTronLaiHinh.Size = new System.Drawing.Size(112, 45);
+            this.btnTronLaiHinh.TabIndex = 2;
+            this.btnTronLaiHinh.Text = "Trộn lại";
+            this.btnTronLaiHinh.UseVisualStyleBackColor = true;
+            this.btnTronLaiHinh.Click += new System.EventHandler(this.btnTronLaiHinh_Click);
+            // 
+            // btnChonHinh
+            // 
+            this.btnChonHinh.Location = new System.Drawing.Point(662, 426);
+            this.btnChonHinh.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnChonHinh.Name = "btnChonHinh";
+            this.btnChonHinh.Size = new System.Drawing.Size(112, 45);
+            this.btnChonHinh.TabIndex = 3;
+            this.btnChonHinh.Text = "Chọn hình";
+            this.btnChonHinh.UseVisualStyleBackColor = true;
+            this.btnChonHinh.Click += new System.EventHandler(this.btnChonHinh_Click);
+            // 
+            // lblHinhGoc
+            // 
+            this.lblHinhGoc.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblHinhGoc.Location = new System.Drawing.Point(474, 118);
+            this.lblHinhGoc.Name = "lblHinhGoc";
+            this.lblHinhGoc.Size = new System.Drawing.Size(300, 300);
+            this.lblHinhGoc.TabIndex = 4;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // lblKiemTra
+            // 
+            this.lblKiemTra.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblKiemTra.Location = new System.Drawing.Point(474, 18);
+            this.lblKiemTra.Name = "lblKiemTra";
+            this.lblKiemTra.Size = new System.Drawing.Size(300, 87);
+            this.lblKiemTra.TabIndex = 5;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(826, 485);
+            this.Controls.Add(this.lblKiemTra);
+            this.Controls.Add(this.lblHinhGoc);
+            this.Controls.Add(this.btnChonHinh);
+            this.Controls.Add(this.btnTronLaiHinh);
+            this.Controls.Add(this.pnXepHinh);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Game xếp hình";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnXepHinh;
+        private System.Windows.Forms.Button btnTronLaiHinh;
+        private System.Windows.Forms.Button btnChonHinh;
+        private System.Windows.Forms.Label lblHinhGoc;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label lblKiemTra;
+    }
+}
---
+++ GameXepHinh/GameXepHinh/frmMain.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameXepHinh
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+        static int KichThucGame = 3; // Game xep hinh 3x3, 5x5,...thêm textbox nếu muốn tùy chình trên giao diện.     
+        private QuanLyGame QuanLy = new QuanLyGame(KichThucGame);
+        private OTrong OTr = null;
+        Button[,] arrButton = null;
+        private void btnChonHinh_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = openFileDialog1.FileName;
+                Bitmap bmp = new Bitmap(fileName);
+                Bitmap newImage = null;
+                newImage = QuanLy.ResizeBitmap(bmp, 300, 300);
+                lblHinhGoc.Image = newImage;
+                newImage = QuanLy.ResizeBitmap(bmp, 400, 400);
+                QuanLy.TaoDuLieuMangHinh(newImage);
+                HienThilenPanel();
+            }
+        }
+        private void HienThilenPanel()
+        {
+            arrButton = new Button[KichThucGame, KichThucGame];
+            OTr = QuanLy.LayGiaTriOTrong();
+            pnXepHinh.Controls.Clear();
+            for(int i = 0; i < KichThucGame; i++)
+            {
+                for(int j = 0; j < KichThucGame; j++)
+                {
+                    Button btn = new Button();
+                    btn.Width = btn.Height = QuanLy.DsBitMap[0].Width;
+                    if (OTr.dong == i && OTr.cot == j)
+                    {
+                        btn.Image = null;
+                    }
+                    else
+                    {
+                        btn.Image = QuanLy.dic.Values.ElementAt(i * KichThucGame + j);
+                    }
+                    btn.Tag = i + ";" + j + ";" + QuanLy.dic.Keys.ElementAt(i * KichThucGame + j);
+                    btn.Location = new Point(j * btn.Width, i * btn.Height);
+                    pnXepHinh.Controls.Add(btn);
+                    arrButton[i, j] = btn;
+                    btn.Click += Btn_Click1;
+                }
+            }
+            kiemtrastt(arrButton);
+        }
+
+        private void Btn_Click1(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            string[] arr = btn.Tag.ToString().Split(';');
+            int dong = int.Parse(arr[0]);
+            int cot = int.Parse(arr[1]);
+            int ViTri = int.Parse(arr[2]);
+            if (OChonCoNamKeBenOTrongKhong(OTr.dong,OTr.cot, dong, cot))
+            {
+                arrButton[OTr.dong, OTr.cot].Image = arrButton[dong, cot].Image;
+                arrButton[OTr.dong, OTr.cot].Tag = OTr.dong + ";" + OTr.cot + ";" + ViTri;
+                arrButton[dong, cot].Image = null;
+                arrButton[dong, cot].Tag = dong + ";" + cot + ";" + OTr.ViTri;
+                OTr.dong = dong;
+                OTr.cot = cot;
+                kiemtrastt(arrButton);
+                if (KiemTraChienThang(arrButton))
+                {
+                    MessageBox.Show("WIN");
+                }
+            }
+        }
+        private void kiemtrastt(Button[,] arr)
+        {
+            string s = "";
+            for(int i = 0; i < arr.GetLength(0); i++)
+            {
+                for(int j=0; j < arr.GetLength(1); j++)
+                {
+                    string[] arrS = arr[i, j].Tag.ToString().Split(';');
+                    s += arrS[2] + "-";
+                }
+                s = s + "\n";
+            }
+            lblKiemTra.Text = s;
+        }
+
+        private bool OChonCoNamKeBenOTrongKhong(int dongTrong, int cotTrong, int dong, int cot)
+        {
+            if (dongTrong - 1 == dong && cotTrong == cot) //nằm trên ô trống.
+            {
+                return true;
+            }
+            if (dongTrong + 1 == dong && cotTrong == cot) //nằm dưới ô trống.
+            {
+                return true;
+            }
+            if (dongTrong == dong && cotTrong - 1 == cot) // nằm bên trái ô trống.
+            {
+                return true;
+            }
+            if (dongTrong == dong && cotTrong + 1 == cot)//nằm bên phải ô trống.
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private bool KiemTraChienThang(Button[,] arr)
+        {
+            bool ChienThang = true; //nếu vi trí của các hình được xếp liên tục từ 1-25 thì ngươi chơi xếp đúng. chiến thắng.
+            for(int i = 0; i < arr.GetLength(0); i++)
+            {
+                for(int j = 0; j < arr.GetLength(1); j++)
+                {
+                    string[] s = arr[i, j].Tag.ToString().Split(';');
+                    int n = int.Parse(s[2]);// vị trí thật của hình khi cắt.
+                    if ((i * KichThucGame + j) != n)
+                    {
+                        ChienThang = false;
+                        break;
+                    }
+                }
+            }
+            return ChienThang;
+        }
+
+        private void btnTronLaiHinh_Click(object sender, EventArgs e)
+        {
+            QuanLy.TaoDsRandomBitMap();
+            HienThilenPanel();
+        }
+    }
+}

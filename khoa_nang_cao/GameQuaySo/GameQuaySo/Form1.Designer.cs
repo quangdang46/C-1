cf2fafc6227ab5a18ad0843252a0601838ad06f6
Trần Quang Đãng <91641923+quangdang46@users.noreply.github.com>
2022-05-12 00:37:21 +0700

    Add files via upload

---
+++ khoa_nang_cao/GameQuaySo/GameQuaySo/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace GameQuaySo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblMay = new System.Windows.Forms.Label();
+            this.lblNguoi = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btnQuay = new System.Windows.Forms.Button();
+            this.btnGameMoi = new System.Windows.Forms.Button();
+            this.btnKetThuc = new System.Windows.Forms.Button();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lbl3);
+            this.panel1.Controls.Add(this.lbl2);
+            this.panel1.Controls.Add(this.lbl1);
+            this.panel1.Location = new System.Drawing.Point(13, 13);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(600, 125);
+            this.panel1.TabIndex = 0;
+            // 
+            // lbl1
+            // 
+            this.lbl1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.lbl1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.ForeColor = System.Drawing.Color.Red;
+            this.lbl1.Location = new System.Drawing.Point(16, 20);
+            this.lbl1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(149, 89);
+            this.lbl1.TabIndex = 0;
+            this.lbl1.Text = "7";
+            this.lbl1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl3
+            // 
+            this.lbl3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.lbl3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbl3.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl3.ForeColor = System.Drawing.Color.Red;
+            this.lbl3.Location = new System.Drawing.Point(432, 20);
+            this.lbl3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(149, 89);
+            this.lbl3.TabIndex = 0;
+            this.lbl3.Text = "7";
+            this.lbl3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lbl3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(78, 154);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(131, 40);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Tền máy:";
+            // 
+            // lblMay
+            // 
+            this.lblMay.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblMay.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMay.ForeColor = System.Drawing.Color.Blue;
+            this.lblMay.Location = new System.Drawing.Point(313, 154);
+            this.lblMay.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblMay.Name = "lblMay";
+            this.lblMay.Size = new System.Drawing.Size(213, 40);
+            this.lblMay.TabIndex = 1;
+            this.lblMay.Text = "100";
+            this.lblMay.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblNguoi
+            // 
+            this.lblNguoi.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblNguoi.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNguoi.ForeColor = System.Drawing.Color.Blue;
+            this.lblNguoi.Location = new System.Drawing.Point(313, 194);
+            this.lblNguoi.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblNguoi.Name = "lblNguoi";
+            this.lblNguoi.Size = new System.Drawing.Size(213, 40);
+            this.lblNguoi.TabIndex = 1;
+            this.lblNguoi.Text = "100";
+            this.lblNguoi.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label8
+            // 
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(18, 194);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(211, 40);
+            this.label8.TabIndex = 1;
+            this.label8.Text = "Tiền người chơi:";
+            // 
+            // btnQuay
+            // 
+            this.btnQuay.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnQuay.Location = new System.Drawing.Point(25, 255);
+            this.btnQuay.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnQuay.Name = "btnQuay";
+            this.btnQuay.Size = new System.Drawing.Size(100, 28);
+            this.btnQuay.TabIndex = 2;
+            this.btnQuay.Text = "Quay số";
+            this.btnQuay.UseVisualStyleBackColor = true;
+            this.btnQuay.Click += new System.EventHandler(this.btnQuay_Click);
+            // 
+            // btnGameMoi
+            // 
+            this.btnGameMoi.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGameMoi.Location = new System.Drawing.Point(267, 255);
+            this.btnGameMoi.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnGameMoi.Name = "btnGameMoi";
+            this.btnGameMoi.Size = new System.Drawing.Size(100, 28);
+            this.btnGameMoi.TabIndex = 2;
+            this.btnGameMoi.Text = "Game mới";
+            this.btnGameMoi.UseVisualStyleBackColor = true;
+            this.btnGameMoi.Click += new System.EventHandler(this.btnGameMoi_Click);
+            // 
+            // btnKetThuc
+            // 
+            this.btnKetThuc.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnKetThuc.Location = new System.Drawing.Point(513, 255);
+            this.btnKetThuc.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnKetThuc.Name = "btnKetThuc";
+            this.btnKetThuc.Size = new System.Drawing.Size(100, 28);
+            this.btnKetThuc.TabIndex = 2;
+            this.btnKetThuc.Text = "Kết thúc";
+            this.btnKetThuc.UseVisualStyleBackColor = true;
+            this.btnKetThuc.Click += new System.EventHandler(this.btnKetThuc_Click);
+            // 
+            // lbl2
+            // 
+            this.lbl2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.lbl2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbl2.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl2.ForeColor = System.Drawing.Color.Red;
+            this.lbl2.Location = new System.Drawing.Point(224, 20);
+            this.lbl2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(149, 89);
+            this.lbl2.TabIndex = 0;
+            this.lbl2.Text = "7";
+            this.lbl2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(637, 306);
+            this.Controls.Add(this.btnKetThuc);
+            this.Controls.Add(this.btnGameMoi);
+            this.Controls.Add(this.btnQuay);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.lblNguoi);
+            this.Controls.Add(this.lblMay);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button btnQuay;
+        private System.Windows.Forms.Button btnGameMoi;
+        private System.Windows.Forms.Button btnKetThuc;
+        private System.Windows.Forms.Timer timer1;
+        public System.Windows.Forms.Label lblMay;
+        public System.Windows.Forms.Label lblNguoi;
+    }
+}
+
---
+++ khoa_nang_cao/GameQuaySo/GameQuaySo/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameQuaySo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        Random rd = new Random();
+        int tienMay =100;
+        int tienNguoi = 100;
+        int cnt = 0;
+        int so1;
+        int so2;
+        int so3;
+        private void btnQuay_Click(object sender, EventArgs e)
+        {
+            tienMay = int.Parse(lblMay.Text);
+            tienNguoi = int.Parse(lblNguoi.Text);
+            if (tienNguoi >= 30)
+            {
+                timer1.Start();
+                tienNguoi -= 30;
+                tienMay += 30;
+                lblNguoi.Text = tienNguoi.ToString();
+                lblMay.Text = tienMay.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Bạn không đủ tiền");
+            }
+
+        }
+
+        private void btnKetThuc_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnGameMoi_Click(object sender, EventArgs e)
+        {
+            lblNguoi.Text = "100";
+            lblMay.Text = "100";
+            lbl1.Text = "7";
+            lbl2.Text = "7";
+            lbl3.Text = "7";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            int so1 = rd.Next(0, 9);
+            int so2 = rd.Next(0, 10);
+            int so3 = rd.Next(0, 11);
+            lbl1.Text = so1.ToString();
+            lbl2.Text = so2.ToString();
+            lbl3.Text = so3.ToString();
+            if (cnt > 100)
+            {
+                if (so1 == 7)
+                {
+                    tienNguoi += 100 + (int)0.5 * tienMay;
+                    tienMay -= 100 + (int)0.5 * tienMay;
+                }
+                if (so2 == 7)
+                {
+                    tienNguoi += 30 + (int)0.5 * tienMay;
+                    tienMay -= 30 + (int)0.5 * tienMay;
+                }
+                if (so3 == 7)
+                {
+                    tienNguoi += 10;
+                    tienMay -= 10;
+                }
+                lblNguoi.Text = tienNguoi.ToString();
+                lblMay.Text = tienMay.ToString();
+
+                timer1.Stop();
+                cnt = 0;
+            }
+            cnt++;
+        }
+    }
+}

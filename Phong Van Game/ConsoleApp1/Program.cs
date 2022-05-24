4115f45eaa8ee22122c0464afbf1c8300679cb9a
Trần Quang Đãng <91641923+quangdang46@users.noreply.github.com>
2022-05-24 20:27:56 +0700

    Add files via upload

---
+++ Phong Van Game/ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 1, 4, 4, 2, 2, 5, 5, 5, 3 };
+            //sort
+            Array.Sort(array);
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (dict.ContainsKey(array[i]))
+                {
+                    dict[array[i]]++;
+                }
+                else
+                {
+                    dict.Add(array[i], 1);
+                }
+            }
+            int len = dict.Values.Max();
+            int[] arr = dict.Keys.ToArray();
+            for(int i = 0; i < arr.Length - 1; i++)
+            {
+                if (Math.Abs(arr[i] - arr[i + 1]) <= 1)
+                {
+                    len = Math.Max(len, dict[arr[i]] + dict[arr[i + 1]]);
+                }
+            }
+            Console.Write(len);
+            Console.ReadKey();
+        }
+    }
+}
